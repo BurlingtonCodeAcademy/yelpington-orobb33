0e867c497f83e620ba564cf78b8d72793c6b9678
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-03-15 13:07:21 -0400

    heroku

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("./client/public"));
+// app.use(express.static("./client/build"));
+const staticDir = path.resolve("./client/build");
+
 
 app.get("/api", (req, res) => {
   res.sendFile(path.resolve("./api/directory.json"));

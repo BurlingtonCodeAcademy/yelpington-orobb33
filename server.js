dc7383912c870c21e66747eef05f47285cdfe904
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-03-15 13:30:35 -0400

    hello

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// app.use(express.static("./client/build"));
+app.use(express.static(staticDir));
 const staticDir = path.resolve("./client/build");
 
 

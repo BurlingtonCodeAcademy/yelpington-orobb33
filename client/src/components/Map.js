fbab78e6a20044c1c33bfdc489b8f420db7b914d
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-03-19 14:51:49 -0400

    professional

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -14,29 +14,22 @@ import "./style/App.css";
 import { Link } from "react-router-dom";
 import MyComponent from "./MyComponent";
 // my component is sibling component to allow zoom
-import glass from '../img/kisspng-wine-glass-martini-cocktail-cosmopolitan-margarita-cocktail-glass-vector-5b224f94e603c1.3837636015289752529422.png'
-
-
+import glass from "../img/kisspng-wine-glass-martini-cocktail-cosmopolitan-margarita-cocktail-glass-vector-5b224f94e603c1.3837636015289752529422.png";
 
 //changing the icon on the map
 let glassIcon = L.icon({
   iconUrl: glass,
-  iconSize:     [38, 50], 
-  shadowSize:   [50, 64], 
-  iconAnchor:   [22, 94], 
-  shadowAnchor: [4, 62],  
-  popupAnchor:  [-3, -76] 
+  iconSize: [38, 50],
+  shadowSize: [50, 64],
+  iconAnchor: [22, 94],
+  shadowAnchor: [4, 62],
+  popupAnchor: [-3, -76],
 });
-
-
-
-
-
 
 export default function Map(props) {
   const [center, setCenter] = useState([44.4759, -73.2121]);
-//use state for default center
-//map settings
+  //use state for default center
+  //map settings
   return (
     <div id="Map">
       <MapContainer
@@ -61,7 +54,10 @@ export default function Map(props) {
           </Popup>
         </Marker>
 
-        <Marker position={[44.47719192504883, -73.21509552001953]} icon={glassIcon}>
+        <Marker
+          position={[44.47719192504883, -73.21509552001953]}
+          icon={glassIcon}
+        >
           <Popup>
             <Link to={`/bars/deli-126`}>Deli 126</Link>
           </Popup>
@@ -79,13 +75,19 @@ export default function Map(props) {
           </Popup>
         </Marker>
 
-        <Marker position={[44.47576141357422, -73.212646484375]} icon={glassIcon}>
+        <Marker
+          position={[44.47576141357422, -73.212646484375]}
+          icon={glassIcon}
+        >
           <Popup>
             <Link to={`/bars/manhattans`}>Manhattan's</Link>
           </Popup>
         </Marker>
 
-        <Marker position={[44.476688385009766, -73.2142562866211]} icon={glassIcon}>
+        <Marker
+          position={[44.476688385009766, -73.2142562866211]}
+          icon={glassIcon}
+        >
           <Popup>
             <Link to={`/bars/monarch`}>Monarch </Link>
           </Popup>
@@ -111,7 +113,7 @@ export default function Map(props) {
 
         <Marker position={[44.48034, -73.21171]} icon={glassIcon}>
           <Popup>
-            <Link to={`/bars/three-needs`}> </Link>
+            <Link to={`/bars/three-needs`}>Three Needs</Link>
           </Popup>
         </Marker>
       </MapContainer>
@@ -119,4 +121,4 @@ export default function Map(props) {
   );
 }
 //my component is sibling allowing for zoom change
-//markers and popups allow the map markers with the links to their page. so you can click to the left on the name, or to the right on the marker link. 
+//markers and popups allow the map markers with the links to their page. so you can click to the left on the name, or to the right on the marker link.

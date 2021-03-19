a8714038538fbc0f5ab60ab96e24f53ad3995d60
Olivia Robb <orobb@Olivias-MacBook-Pro.local>
2021-03-19 13:08:43 -0400

    fix

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const staticDir = path.resolve("./client/build");
 
 app.use(express.static(staticDir));
-const staticDir = path.resolve("./client/build");
+
 
 app.get("/api", (req, res) => {
   res.sendFile(path.resolve("./api/directory.json"));
